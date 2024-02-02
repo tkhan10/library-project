3f8dd9bbea7551e81b6d05d1f85062b746337243
tofekkhan <tofeequekhan87@gmail.com>
2024-02-01 22:32:13 -0600

    Initial commit

---
+++ src/main/java/com/tofek/practice/LibrarayProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tofek.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarayProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarayProjectApplication.class, args);
+	}
+
+}
